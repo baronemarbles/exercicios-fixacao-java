84230395afcc58b35b970b301acf93aa4d4a841a
baroneMarcio <baronempbb@gmail.com>
2026-08-10 14:39:04 -0300

    Atributos do Pai, mãe e criacao do fator 'sorte' na escolha do genotipo da cria e na escolha de genero do individuo

--- grandesfelinos/OncaPintada.java
+++ grandesfelinos/OncaPintada.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.grandesfelinos;
 import java.util.ArrayList;
+import java.util.Random;
 import java.lang.StringBuilder;
 /**
  *
@@ -12,12 +13,15 @@ import java.lang.StringBuilder;
 
 public class OncaPintada extends Panthera implements Procriacao{
     public String nome;
+    public String pai;
+    public String mae;
     protected float idade=0;
     protected String sexo;
     protected String pelagem;
     protected String fenotipoPelo= "Aa";//padrao, com chance de haver diversidade
     protected String aleloDominantePelo="A"; //O níveis considerados naturais de melanina nos indivíduos dessa espécie o caracterizam como amarelo claro com rosetas escuras
     private StringBuilder builder = new StringBuilder();
+    private Random rand = new Random();
 //    private boolean isGravida=false;
     public String getNome(){
         return this.nome;
@@ -25,6 +29,22 @@ public class OncaPintada extends Panthera implements Procriacao{
     
     public void setNome(String nome){
         this.nome=nome;
+    }
+    
+    public String getMae(){
+        return this.mae;
+    }
+    
+    private void setMae(String nomeMae){
+        this.mae=nomeMae;
+    }
+    
+    public String getPai(){
+        return this.pai;
+    }
+    
+    private void setPai(String nomePai){
+        this.pai=nomePai;
     }
     
     public float getIdade(){
@@ -80,6 +100,8 @@ public class OncaPintada extends Panthera implements Procriacao{
         this.idade=0;
         this.pelagem="Amarela clara com Rosetas castanhas e avermelhadas";
         forcaMordida=2000;
+        this.pai="Grande criador";
+        this.mae="Grande criador";
     }
     
     
@@ -111,89 +133,70 @@ public class OncaPintada extends Panthera implements Procriacao{
             char[] alelosMae = this.getFenotipoPelo().toCharArray();
             ArrayList<Character> alelosCria = new ArrayList<Character>();
             
-                alelosCria.add(alelosPai[0]);
-                alelosCria.add(alelosMae[0]);
-//                alelosCria.add(',');
-                alelosCria.add(alelosPai[0]);
-                alelosCria.add(alelosMae[1]);
-//                alelosCria.add(',');
-                alelosCria.add(alelosPai[1]);
-                alelosCria.add(alelosMae[0]);
-//                alelosCria.add(',');
-                alelosCria.add(alelosPai[1]);
-                alelosCria.add(alelosMae[1]);
+                builder.append(alelosPai[0]);
+                builder.append(alelosMae[0]);
+                alelosCriaPossiveis.add(builder.toString()) ;
+                builder.setLength(0);
                 
-                String alelosCriaLiteral= alelosCria.toString();
-                for(int i=0;i<alelosCria.size();i++){
-                    System.out.format("Alelo cria: %c\n", alelosCria.get(i));
-                }
+                builder.append(alelosPai[0]);
+                builder.append(alelosMae[1]);
+                System.out.println("Objeto do builder depois do primeiro setLenght(0): "+builder.toString());
+                alelosCriaPossiveis.add(builder.toString());
+                System.out.println("Objeto dos alelos possiveis após 2 iterações do chuveirinho entre fenotipos: "+alelosCriaPossiveis);
+                builder.setLength(0);
                 
-                System.out.format("Apos conversao para String: %s\n", alelosCriaLiteral);
-                System.out.format("Apos conversao para String: %c\n", alelosCriaLiteral.charAt(0));
-                System.out.format("Apos conversao para String: %c\n", alelosCriaLiteral.charAt(1));
-                System.out.println("Clear na arraylist<Character> dos alelosCria efetuado");
-                for(int i=0;i<alelosCriaLiteral.length();i++){
-                    if(i<alelosCriaLiteral.length()-1){
-                        if(alelosCriaLiteral.charAt(i)==','){
-                        System.out.println("Bypass no loop será executado devido a ser uma virgula!");    
-                        continue;
-                        }
-                        else{
-                            builder.append(alelosCriaLiteral.charAt(i));
-                            builder.append(alelosCriaLiteral.charAt(i+1));
-//                            var listOfAlelos = new ArrayList<String>();
-                            alelosCriaPossiveis.add(builder.toString());
-//                            builder.setLength(0);
-                            i=i+2;
-                        }
-                    }
+                builder.append(alelosPai[1]);
+                builder.append(alelosMae[0]);
+                System.out.println("Objeto do builder depois do segundo setLenght(0): "+builder.toString());
+                alelosCriaPossiveis.add(builder.toString());
+                System.out.println("Objeto dos alelos possiveis após 2 iterações do chuveirinho entre fenotipos: "+alelosCriaPossiveis);
+                builder.setLength(0);
+                
+                builder.append(alelosPai[1]);
+                builder.append(alelosMae[1]);
+                System.out.println("Objeto do builder depois do segundo setLenght(0): "+builder.toString());
+                alelosCriaPossiveis.add(builder.toString());
+                System.out.println("Objeto dos alelos possiveis após 3 iterações do chuveirinho entre fenotipos: "+alelosCriaPossiveis);
+                
+                //Advindo da sorte na formação de um embrião
+                String nomeCria= "cria"+this.getNome()+parceiro.getNome();
+                String generoCria;
+                
+                if(rand.nextInt(1)==1){//0 é femea, 1 é macho.
+                    generoCria="macho";
+                } else {generoCria="femea";}
+                OncaPintada cria = new OncaPintada();
+                cria.setNome(nomeCria);
+                cria.setMae(this.nome);
+                cria.setPai(parceiro.nome);
+                cria.setFenotipoPelo(alelosCriaPossiveis.get(rand.nextInt(4)));
+                cria.verificarPelagem();
+                cria.setSexo(generoCria);
+                System.out.printf("\n%s nasceu, é %s e é cria de %s com %s.\nSeu fenotipo de pelo é '%s'.\nTem pelagem %s.\nTem %s anos.\n", cria.getNome(),cria.getSexo(),cria.getMae(),cria.getPai(),cria.getFenotipoPelo(),cria.getPelagem(),cria.getIdade());
+                
+                
+                
+                
+                
+                
+        
                     
-                    
-                }
+        }
                 
-            for (String alelos : alelosCriaPossiveis) {
-                System.out.format("Lista dos genóticos de pelagem possiveis: %s\n", alelos );
-            }
+//            for (String alelos : alelosCriaPossiveis) {
+//                System.out.format("Lista dos genóticos de pelagem possiveis: %s\n", alelos );
+//            }
 
 //              for(int i=0;i<alelosCriaPossiveis.size();i++){
 //                  System.out.format("Lista dos genóticos de pelagem possiveis:s\n", alelosCriaPossiveis.get(i));
-//              }
-            /*
-            while(i<2){
-            if(i==1){
-            alelosCria.add(alelosPai[i]);
-            alelosCria.add(alelosMae[0]);
-            alelosCria.add(',');
-            alelosCria.add(alelosPai[i]);
-            alelosCria.add(alelosMae[0]);
-            }
-            else{
-            alelosCria.add(alelosPai[i]);
-            alelosCria.add(alelosMae[0]);
-            alelosCria.add(',');
-            alelosCria.add(alelosPai[i]);
-            alelosCria.add(alelosMae[1]);
-            alelosCria.add(',');
-            }
-            i++;
-            //                alelosCria.add(alelosPai[0]);
-            //                alelosCria.add(alelosMae[1]);
-            //                alelosCria.add(',');
-            //                alelosCria.add(alelosPai[1]);
-            //                alelosCria.add(alelosMae[0]);
-            //                alelosCria.add(',');
-            //                alelosCria.add(alelosPai[1]);
-            //                alelosCria.add(alelosMae[1]);
-            }
-             */
+//              }          
             //armazenar possibilidades no array, escolha aleatoria de uma das possibilidades considerando a probabilidade e escolha randomica de genero
-            
-            System.out.println(alelosCriaPossiveis+"\n");
-        } else{
+
+         else{
             System.out.println("Não foi possível, a reprodução desta espécie só ocorre entre um macho e uma femea");
         }
         
-        System.out.println(alelosCriaPossiveis.get(0));
+//        System.out.println(alelosCriaPossiveis.get(0));
 //apenas a femea pode dar a luz, portanto a cupula acontece nela e é gerada através dela, função vai ser dela
     }
     
